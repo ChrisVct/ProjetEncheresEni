f4ae51689114f7bc0d53955737d817a5e5247797
Robert <bobi25@hotmail.fr>
2022-07-20 16:45:47 +0200

    Uc "s'inscrire":DAL,(BLL 1/2)

--- src/main/java/fr/eni/projetEncheres/dal/DAO.java
+++ src/main/java/fr/eni/projetEncheres/dal/DAO.java
@@ -1,11 +1,12 @@
 package fr.eni.projetEncheres.dal;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import fr.eni.projetEncheres.BusinessException;
 
 public interface DAO<T> {
-	public void insert(T t);
+	public void insert(T t) throws BusinessException ;
 	public List<T> selectAll() throws BusinessException;
 	public T selectByID(int id);
 	public void update(T t);
