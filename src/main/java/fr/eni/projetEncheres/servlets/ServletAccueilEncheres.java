b6c045a2773519189dc12ca697f7cea7050417ab
willi <willi@DESKTOP-TFE0U58.mshome.net>
2022-07-29 11:33:47 +0200

    UC 2005 - Affichage des éléments d'enchère par leur getters

--- src/main/java/fr/eni/projetEncheres/servlets/ServletAccueilEncheres.java
+++ src/main/java/fr/eni/projetEncheres/servlets/ServletAccueilEncheres.java
@@ -39,54 +39,31 @@ public class ServletAccueilEncheres extends HttpServlet {
 			EnchereManager eManager = EnchereManager.getInstance();
 			listeEncheres = eManager.afficherEncheresOuvertes();
 		} catch (BusinessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		System.out.println(listeEncheres);
 		request.setAttribute("listeEncheres", listeEncheres);
-		System.out.println(request.getSession().getAttribute("utilisateur_connecte"));
 		rd = request.getRequestDispatcher("/WEB-INF/JSP/AccueilEncheres.jsp");
 		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if(request.getAttribute("connexion")==null) {
-			//Récuperer les données du formulaire
 			String nomArticle = request.getParameter("nomArticle");
 			String libelle = request.getParameter("libelle");
 			List<Enchere> listeEncheres = new ArrayList<>();
 			List<Enchere> listeNulle = null;
-			//Envoi des données à la BLL
 			try {
 				EnchereManager eManager = EnchereManager.getInstance();
-				//déclarer une liste(tableau) (listeEnchere) + (afficherEncheresAvecParametres)
 				listeEncheres = eManager.afficherEncheresAvecParametres(nomArticle, libelle, listeNulle);
-				//Retourner la liste à la JSP
 				request.setAttribute("listeEncheres", listeEncheres);
 			} catch (BusinessException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			RequestDispatcher rd = request.getServletContext().getRequestDispatcher("/WEB-INF/JSP/AccueilEncheres.jsp");
 			rd.forward(request, response);
 			
 		}else if(request.getAttribute("connexion")!=null){
-			System.out.println("je passe dans e esleif");
 			doGet(request, response);
 		}
-	
-		//int ??noUtilisateur?? = ((Utilisateur)request.getSession().getAttribute("utilisateur_connecte")).getNoUtilisateur();
-		//si tu recois ... tu fais ...
-//		getinstance
-//		if(request.getParameter("enchereouverte") !null
-//				emanager.afficherEncheresOuvertes()
-//				
-//				if(request.getParameter("ecobyID") !null
-//						if(request.getParameter("ecobyID")
-//						emanager.afficherAchatencours()
-		
-		//recuperer le cas ou mes encheres en cours sont cochés => lancer fonction en bll
-				
-		
 	}
 }
